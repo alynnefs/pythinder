3054b9431aef725b2d374ab3b84f338c47f9dacd
Alynne Ferreira <alynneferreiras@gmail.com>
2022-08-21 22:46:49 -0300

    ajustes de cor e alinhamento

--- app/pythinder.py
+++ app/pythinder.py
@@ -5,7 +5,7 @@ from kivymd.app import MDApp
 class Pythinder(MDApp):
     def build(self):
         self.theme_cls.theme_style = "Light"
-        self.theme_cls.primary_palette = "Green"
+        self.theme_cls.primary_palette = "Blue"
         return Builder.load_file('pythinder.kv')
 
 Pythinder().run()
